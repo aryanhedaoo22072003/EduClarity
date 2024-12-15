ebb25810770b9bbdee29adc279daa12e53094b6a
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-15 20:34:15 +0530

    Database connection

--- server/server.ts
+++ server/server.ts
@@ -1,7 +1,9 @@
 import { app } from "./app";
+import connectDB from "./utils/db";
 require("dotenv").config();
 
 //CRETA server
 app.listen(process.env.PORT,()=>{
     console.log(`Server is connected with Port ${process.env.PORT}`);
+    connectDB();
 });
