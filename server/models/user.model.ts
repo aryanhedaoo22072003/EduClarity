cc2a6c851ef9898f592eec42f83c1a1a57771241
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-16 23:31:11 +0530

    Login and Logout User

--- server/models/user.model.ts
+++ server/models/user.model.ts
@@ -1,5 +1,7 @@
 import mongoose, { Document, Model, Schema } from "mongoose";
 import bcrypt from "bcryptjs";
+require("dotenv").config();
+import jwt from "jsonwebtoken";
 
 const emailRegexPattern: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 
@@ -15,71 +17,83 @@ export interface IUser extends Document {
   isVerified: boolean;
   courses: Array<{ courseId: string }>;
   comparePassword: (password: string) => Promise<boolean>;
+  SignAccessToken: () => string;
+  SignRefreshToken: () => string;
 }
 
-
-
 const userSchema: Schema<IUser> = new mongoose.Schema(
-    {
-      name: {
-        type: String,
-        required: [true, "Please enter your name"],
-      },
-      email: {
-        type: String,
-        required: [true, "Please enter your email"],
-        validate: {
-          validator: function (value: string) {
-            return emailRegexPattern.test(value);
-          },
-          message: "please enter a valid email",
-        },
-        unique: true,
-      },
-      password: {
-        type: String,
-        required: [true, "Please enter your password"],
-        minlength: [6, "Password must be at least 6 caracters"],
-        select:false,
-      },
-      avatar: {
-        public_id: String,
-        url: String,
-      },
-      role: {
-        type: String,
-        default: "user",
-      },
-      isVerified: {
-        type: Boolean,
-        default: false,
-      },
-      courses: [
-        {
-          courseId: String,
-        },
-      ],
+  {
+    name: {
+      type: String,
+      required: [true, "Please enter your name"],
     },
-    { timestamps: true }
-  );
-
+    email: {
+      type: String,
+      required: [true, "Please enter your email"],
+      validate: {
+        validator: function (value: string) {
+          return emailRegexPattern.test(value);
+        },
+        message: "please enter a valid email",
+      },
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: [true, "Please enter your password"],
+      minlength: [6, "Password must be at least 6 caracters"],
+      select: false,
+    },
+    avatar: {
+      public_id: String,
+      url: String,
+    },
+    role: {
+      type: String,
+      default: "user",
+    },
+    isVerified: {
+      type: Boolean,
+      default: false,
+    },
+    courses: [
+      {
+        courseId: String,
+      },
+    ],
+  },
+  { timestamps: true }
+);
 
 //hash password before saving
 userSchema.pre<IUser>("save", async function (next) {
-    if (!this.isModified("password")) {
-      next();
-    }
-    this.password = await bcrypt.hash(this.password, 10);
+  if (!this.isModified("password")) {
     next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+  next();
+});
+
+//sign access token
+userSchema.methods.SignAccessToken = function () {
+  return jwt.sign({ id: this._id }, process.env.ACCESS_TOKEN || "", {
+    expiresIn: "5m",
   });
-  
+};
+
+//sign refresh token
+userSchema.methods.SignRefreshToken = function () {
+  return jwt.sign({ id: this._id }, process.env.REFRESH_TOKEN || "", {
+    expiresIn: "3d",
+  });
+};
+
 //compare password
 userSchema.methods.comparePassword = async function (
-    enteredPassword: string
-  ): Promise<boolean> {
-    return await bcrypt.compare(enteredPassword, this.password);
-  };
+  enteredPassword: string
+): Promise<boolean> {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
-
-  const userModel: Model<IUser> = mongoose.model("User", userSchema);
-  export default userModel;
+const userModel: Model<IUser> = mongoose.model("User", userSchema);
+export default userModel;
