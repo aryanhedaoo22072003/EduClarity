f31fc252984b7e8f39d2fc23f3e81a1e87862ac2
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-18 22:27:50 +0530

    get all courses

--- server/controllers/course.controller.ts
+++ server/controllers/course.controller.ts
@@ -4,7 +4,7 @@ import ErrorHandler from "../utils/ErrorHandler";
 import cloudinary from "cloudinary";
 import { createCourse } from "../services/course.service";
 import CourseModel from "../models/course.model";
-// import { redis } from "../utils/redis";
+import { redis } from "../utils/redis";
 
 //upload course
 export const uploadCourse = CatchAsyncError(
@@ -67,3 +67,70 @@ export const editCourse = CatchAsyncError(
     }
   }
 );
+
+//get single course--without purchasing
+export const getSingleCourse = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const courseId = req.params.id;
+  
+        const isCacheExist = await redis.get(courseId);
+  
+        //console.log('hitting redis');
+  
+        if (isCacheExist) {
+          const course = JSON.parse(isCacheExist);
+          res.status(200).json({
+            success: true,
+            course,
+          });
+        } else {
+          const course = await CourseModel.findById(req.params.id).select(
+            "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+          );
+          //console.log('hitting mongodb');
+          await redis.set(courseId, JSON.stringify(course));
+  
+          res.status(200).json({
+            success: true,
+            course,
+          });
+        }
+    }
+       catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    }
+  );
+  
+//get all courses -- without purchasing
+export const getAllCourses = CatchAsyncError(
+    async (req: Request, res: Response, next: NextFunction) => {
+      try {
+        const isCacheExist = await redis.get("allCourses");
+        if (isCacheExist) {
+          const courses = JSON.parse(isCacheExist);
+         // console.log("hitting redis");
+          res.status(200).json({
+            success: true,
+            courses,
+          });
+        } else {
+          const courses = await CourseModel.find().select(
+            "-courseData.videoUrl -courseData.suggestion -courseData.questions -courseData.links"
+          );
+          //console.log("hitting mongdb");
+         await redis.set("allCourses",JSON.stringify(courses));
+          res.status(200).json({
+            success: true,
+            courses,
+          });
+        }
+      }
+       catch (error: any) {
+        return next(new ErrorHandler(error.message, 500));
+      }
+    }
+  );
+
+  
