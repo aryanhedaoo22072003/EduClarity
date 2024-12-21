706259b260951ece12455e9421b17ba5857d5e77
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-21 23:51:38 +0530

    Layout for FAQ and Categories

--- server/app.ts
+++ server/app.ts
@@ -9,6 +9,7 @@ import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
 import analyticsRouter from "./routes/analytics.route";
 import notificationRoute from "./routes/notification.route";
+import layoutRouter from "./routes/layout.route";
 //body parser
 app.use(express.json({limit:"50mb"}));
 
@@ -23,7 +24,7 @@ app.use(cors({
 
 
 //routes
-app.use("/api/v1",userRouter,orderRouter,courseRouter,notificationRoute,analyticsRouter);
+app.use("/api/v1",userRouter,orderRouter,courseRouter,notificationRoute,analyticsRouter,layoutRouter);
 
 
 
