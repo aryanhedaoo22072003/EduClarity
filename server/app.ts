5bfc2b88a07d3e3b18609c2ac4ea4e9b8290a1ee
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-21 21:07:22 +0530

    Analysis for orders,courses,users only --for admin

--- server/app.ts
+++ server/app.ts
@@ -7,7 +7,7 @@ import {ErrorMiddleware} from "./middleware/error";
 import userRouter from "./routes/user.route";
 import courseRouter from "./routes/course.route";
 import orderRouter from "./routes/order.route";
-
+import analyticsRouter from "./routes/analytics.route";
 import notificationRoute from "./routes/notification.route";
 //body parser
 app.use(express.json({limit:"50mb"}));
@@ -23,7 +23,7 @@ app.use(cors({
 
 
 //routes
-app.use("/api/v1",userRouter,orderRouter,courseRouter,notificationRoute);
+app.use("/api/v1",userRouter,orderRouter,courseRouter,notificationRoute,analyticsRouter);
 
 
 
