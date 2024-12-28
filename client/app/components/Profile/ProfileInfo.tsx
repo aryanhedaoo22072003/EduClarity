8db723ae4e16cd59db790021fda817e323581113
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-28 19:51:04 +0530

    Edit Profile and update password

--- client/app/components/Profile/ProfileInfo.tsx
+++ client/app/components/Profile/ProfileInfo.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { styles } from "../../../app/styles/style";
 import Image from "next/image";
