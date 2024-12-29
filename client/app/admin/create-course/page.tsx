7290be1d7dfc050f3968b23ae30bbd17a7631f77
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-29 17:20:22 +0530

    Create Course and information for admin

--- client/app/admin/create-course/page.tsx
+++ client/app/admin/create-course/page.tsx
@@ -8,7 +8,22 @@ type Props = {}
 
 const page = (props: Props) => {
   return (
-    <div>page</div>
+    <div>
+        <Heading
+         title="EduClarity - Admin"
+         description="EduClarity is a platform for students to learn and get help from teachers"
+         keywords="Prograaming,MERN,Redux,Machine Learning"
+        />
+        <div className="flex">
+            <div className="1500px:w-[16%] w-1/5">
+                <AdminSidebar />
+            </div>
+            <div className="w-[85%]">
+               <DashboardHeader />
+               <CreateCourse /> 
+            </div>
+        </div>
+    </div>
   )
 }
 
--- client/app/components/Admin/course/CreateCourse.tsx
+++ client/app/components/Admin/course/CreateCourse.tsx
@@ -1,11 +1,92 @@
-import React from 'react'
-
+'use client'
+import React, { useState } from 'react'
+import CourseInformation from './CourseInformation'
+import CourseOptions from './CourseOptions'
 type Props = {}
 
 const CreateCourse = (props: Props) => {
+    const [active,setActive]=useState(0);
+    const [courseInfo, setCourseInfo] = useState({
+        name: "",
+        description: "",
+        price: "",
+        estimatedPrice: "",
+        tags: "",
+        level: "",
+        categories:"",
+        demoUrl: "",
+        thumbnail: "",
+      });
+
+      const [benefits, setBenefits] = useState([{ title: "" }]);
+  const [prerequisites, setPrerequisites] = useState([{ title: "" }]);
+  const [courseContentData, setCourseContentData] = useState([
+    {
+      videoUrl: "",
+      title: "",
+      description: "",
+      videoSection: "Untitled Section",
+      videoLength: "",
+      links: [
+        {
+          title: "",
+          url: "",
+        },
+      ],
+      suggestion: "",
+    },
+  ]);
+
+
+  const [courseData, setCourseData] = useState({});
+
   return (
-    <div>CreateCourse</div>
-  )
-}
+    <div className="w-full flex min-h-screen">
+      <div className="w-[80%]">
+        {active === 0 && (
+          <CourseInformation
+            courseInfo={courseInfo}
+            setCourseInfo={setCourseInfo}
+            active={active}
+            setActive={setActive}
+          />
+        )}
+
+        {/* {active === 1 && (
+          <CourseData
+            benefits={benefits}
+            setBenefits={setBenefits}
+            prerequisites={prerequisites}
+            setPrerequisites={setPrerequisites}
+            active={active}
+            setActive={setActive}
+          />
+        )} */}
+
+        {/* {active === 2 && (
+          <CourseContent
+            active={active}
+            setActive={setActive}
+            courseContentData={courseContentData}
+            setCourseContentData={setCourseContentData}
+            handleSubmit={handleSubmit}
+          />
+        )} */}
+
+        {/* {active === 3 && (
+          <CoursePreview
+            active={active}
+            setActive={setActive}
+            courseData={courseData}
+            handleCourseCreate={handleCourseCreate}
+          />
+        )} */}
+      </div>
+      <div className="w-[20%] mt-[100px] h-screen fixed z-[-1] top-18 right-0">
+        <CourseOptions active={active} setActive={setActive} />
+      </div>
+    </div>
+  );
+};
 
 export default CreateCourse
