e039e990d43b6311cd22442a46eb71585ece051e
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2024-12-31 15:46:33 +0530

    Get all users,courses and manage team

--- client/app/components/Admin/course/AllCourses.tsx
+++ client/app/components/Admin/course/AllCourses.tsx
@@ -22,10 +22,6 @@ const AllCourses = (props: Props) => {
           return (
             <>
               <Button
-                onClick={() => {
-                  setOpen(!open);
-                  setCourseId(params.row.id);
-                }}
               >
                 <AiOutlineDelete
                   className="dark:text-white text-black"
@@ -37,9 +33,108 @@ const AllCourses = (props: Props) => {
         },
       },
     ];
+
+    const rows=[
+        {
+            id:"1234",
+            title:"Course 1",
+            purchased:'30',
+            ratings:'4.5',
+            created_at:'2022-01-01'
+        }
+    ]
   return (
-    <div>AllCourses</div>
-  )
-}
+    <div className="mt-[120px]">
+    {/* // {isLoading ? (
+    //   <Loader />
+    // ) : ( */}
+      <Box m="20px">
+        <Box
+          m="40px 0 0 0"
+          height="80vh"
+          sx={{
+            "& .MuiDataGrid-root": {
+              border: "none",
+              outline: "none",
+            },
+            "& .css-pqjvzy-MuiSvgIcon-root-MuiSelect-icon": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-sortIcon": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-row": {
+              color: theme === "dark" ? "#fff" : "#000",
+              borderBottom:
+                theme === "dark"
+                  ? "1px solid #ffffff30!important"
+                  : "1px solid #ccc!important",
+            },
+            "& .MuiTablePagination-root": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-cell": {
+              borderBottom: "none!important",
+            },
+            "& .name-column--cell": {
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-columnHeaders": {
+              backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
+              borderBottom: "none",
+              color: theme === "dark" ? "#fff" : "#000",
+            },
+            "& .MuiDataGrid-virtualScroller": {
+              backgroundColor: theme === "dark" ? "#1F2A40" : "#F2F0F0",
+            },
+            "& .MuiDataGrid-footerContainer": {
+              color: theme === "dark" ? "#fff" : "#000",
+              borderTop: "none",
+              backgroundColor: theme === "dark" ? "#3e4396" : "#A4A9FC",
+            },
+            "& .MuiCheckbox-root": {
+              color:
+                theme === "dark" ? `#b7ebde !important` : `#000 !important`,
+            },
+            "& .MuiDataGrid-toolbarContainer .MuiButton-text": {
+              color: `#fff !important`,
+            },
+          }}
+        >
+<DataGrid checkboxSelection rows={rows} columns={columns} />
+        {/* </Box> */}
+        {/* {open && (
+          <Modal
+            open={open}
+            onClose={() => setOpen(!open)}
+            aria-labelledby="modal-modal-title"
+            aria-describedby="modal-modal-description"
+          >
+            <Box className="absolute top-[50%] left-[50%] -translate-x-1/2 -translate-y-1/2 w-[450px] bg-white dark:bg-slate-900 rounded-[8px] shadow p-4 outline-none">
+              <h1 className={`${styles.title}`}>
+                Are you sure you want to delete this course?
+              </h1>
+              <div className="flex w-full items-center justify-between mb-6 mt-4">
+                <div
+                  className={`${styles.button} !w-[120px] h-[30px] bg-[#47d097]`}
+                  onClick={() => setOpen(!open)}
+                >
+                  Cancel
+                </div>
+                <div
+                  className={`${styles.button} !w-[120px] h-[30px] bg-[#d63f3f]`}
+                  onClick={handleDelete}
+                >
+                  Delete
+                </div>
+              </div>
+            </Box> */}
+          {/* </Modal>
+        )} */} 
+            </Box>
+        </Box>
+    </div>
+          )
+        }
 
 export default AllCourses;
