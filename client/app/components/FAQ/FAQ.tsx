994269912fa974c1a1bad7e11b4a69271670574d
aryanhedaoo22072003 <aryanhedaoo2003@gmail.com>
2025-01-07 15:56:20 +0530

    Footer section done

--- client/app/components/FAQ/FAQ.tsx
+++ client/app/components/FAQ/FAQ.tsx
@@ -1,18 +1,97 @@
+// /* eslint-disable @typescript-eslint/no-empty-object-type */
+// /* eslint-disable @typescript-eslint/no-unused-vars */
+// /* eslint-disable @typescript-eslint/no-explicit-any */
+// import { styles } from '@/app/styles/style';
+// import { useGetHeroDataQuery } from '@/redux/features/layout/layoutApi';
+// import React, { useEffect, useState } from 'react'
+// import { HiMinus, HiPlus } from 'react-icons/hi';
+
+// type Props = {}
+
+// const FAQ = (props: Props) => {
+//     const { data } = useGetHeroDataQuery("FAQ", {
+//       });
+//       const [activeQuestion, setActiveQuestion] = useState(null);
+//   const [questions, setQuestions] = useState<any[]>([]);
+
+//   useEffect(() => {
+//     if (data) {
+//       setQuestions(data.layout?.faq);
+//     }
+//   }, [data]);
+
+//   const toggleQuestion = (id: any) => {
+//     setActiveQuestion(activeQuestion === id ? null : id);
+//   };
+
+//   return (
+//     <div>
+//          <div className="w-[90%] 800px:w-[80%] m-auto">
+//         <h1 className={`${styles.title} 800px:text-[40px]`}>
+//           Frequently Asked Questions
+//         </h1>
+//         <div className="mt-12">
+//           <dl className="space-y-8">
+//             {questions?.map((q) => (
+//               <div key={q.id}
+//               className={`${
+//                 q._id !== questions[0]?._id && "border-t"
+//               } border-gray-200 pt-6`}
+//               >
+//                 <dt className="text-lg">
+//                   <button
+//                     className="flex items-start justify-between w-full text-left focus:outline-none"
+//                     onClick={() => toggleQuestion(q._id)}
+//                   >
+//                     <span className="font-medium text-black dark:text-white">{q.question}</span>
+//                     <span className="ml-6 flex-shrink-0">
+//                       {activeQuestion === q._id ? (
+//                         <HiMinus className="h-6 w-6 text-black dark:text-white" />
+//                       ) : (
+//                         <HiPlus className="h-6 w-6 text-black dark:text-white" />
+//                       )}
+//                     </span>
+//                   </button>
+//                 </dt>
+//                 {activeQuestion === q._id && (
+//                   <dd className="mt-2 pr-12">
+//                     <p className="text-base font-Poppins text-black dark:text-white">{q.answer}</p>
+//                   </dd>
+//                 )}
+//               </div>
+//             ))}
+//           </dl>
+//         </div>
+//         <br />
+//         <br />
+//         <br />
+//       </div> 
+//     </div>
+//   )
+// }
+
+// export default FAQ
+
 /* eslint-disable @typescript-eslint/no-empty-object-type */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import { styles } from '@/app/styles/style';
 import { useGetHeroDataQuery } from '@/redux/features/layout/layoutApi';
-import React, { useEffect, useState } from 'react'
+import React, { useEffect, useState } from 'react';
 import { HiMinus, HiPlus } from 'react-icons/hi';
 
-type Props = {}
+interface FAQType {
+  _id: string;
+  question: string;
+  answer: string;
+}
+
+type Props = {};
 
 const FAQ = (props: Props) => {
-    const { data } = useGetHeroDataQuery("FAQ", {
-      });
-      const [activeQuestion, setActiveQuestion] = useState(null);
-  const [questions, setQuestions] = useState<any[]>([]);
+  const { data } = useGetHeroDataQuery('FAQ');
+  const [activeQuestion, setActiveQuestion] = useState<string | null>(null);
+  const [questions, setQuestions] = useState<FAQType[]>([]);
 
   useEffect(() => {
     if (data) {
@@ -20,30 +99,33 @@ const FAQ = (props: Props) => {
     }
   }, [data]);
 
-  const toggleQuestion = (id: any) => {
+  const toggleQuestion = (id: string) => {
     setActiveQuestion(activeQuestion === id ? null : id);
   };
 
   return (
     <div>
-         <div className="w-[90%] 800px:w-[80%] m-auto">
+      <div className="w-[90%] 800px:w-[80%] m-auto">
         <h1 className={`${styles.title} 800px:text-[40px]`}>
           Frequently Asked Questions
         </h1>
         <div className="mt-12">
           <dl className="space-y-8">
             {questions?.map((q) => (
-              <div key={q.id}
-              className={`${
-                q._id !== questions[0]?._id && "border-t"
-              } border-gray-200 pt-6`}
+              <div
+                key={q._id} // Use a unique identifier like _id here
+                className={`${
+                  q._id !== questions[0]?._id && 'border-t'
+                } border-gray-200 pt-6`}
               >
                 <dt className="text-lg">
                   <button
                     className="flex items-start justify-between w-full text-left focus:outline-none"
                     onClick={() => toggleQuestion(q._id)}
                   >
-                    <span className="font-medium text-black dark:text-white">{q.question}</span>
+                    <span className="font-medium text-black dark:text-white">
+                      {q.question}
+                    </span>
                     <span className="ml-6 flex-shrink-0">
                       {activeQuestion === q._id ? (
                         <HiMinus className="h-6 w-6 text-black dark:text-white" />
@@ -55,7 +137,9 @@ const FAQ = (props: Props) => {
                 </dt>
                 {activeQuestion === q._id && (
                   <dd className="mt-2 pr-12">
-                    <p className="text-base font-Poppins text-black dark:text-white">{q.answer}</p>
+                    <p className="text-base font-Poppins text-black dark:text-white">
+                      {q.answer}
+                    </p>
                   </dd>
                 )}
               </div>
@@ -65,9 +149,9 @@ const FAQ = (props: Props) => {
         <br />
         <br />
         <br />
-      </div> 
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default FAQ
+export default FAQ;
--- client/app/page.tsx
+++ client/app/page.tsx
@@ -8,6 +8,9 @@ import Hero from "./components/Route/Hero";
 import Courses from "./components/Route/Courses";
 import Reviews from "./components/Route/Reviews";
 import FAQ from "./components/FAQ/FAQ";
+import Footer from "./components/Footer";
+
+
 interface Props {}
 
 const Page: FC<Props> = (props) => {
@@ -32,6 +35,7 @@ const Page: FC<Props> = (props) => {
       <Courses/>
       <Reviews/>
       <FAQ/>
+      <Footer/>
     </div>
   );
 };
